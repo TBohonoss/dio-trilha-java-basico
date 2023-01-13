d46cb568c47b174ebe197b86291c5c1975545f82
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-13 19:43:03 -0300

    Declarando variáveis do tipo de texto e e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        
+        String meuNome = "Thiago Bohonos"; 
+
         double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double PI = 3.14;
+
+        System.out.println(PI);
     }
 }
